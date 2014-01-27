a0010e06227c4892beacd09d7f1736257efe1afc
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-01-28 00:25:15 +0100

    simple page setup bootstrap

--- myproject/settings/common.py
+++ myproject/settings/common.py
@@ -19,6 +19,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'frontend',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from frontend.views import HomePageView
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'myproject.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', HomePageView.as_view(), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
