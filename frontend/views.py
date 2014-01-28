b7aee23804e1a43c37ac662db032e7db6eef6bfb
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-01-28 02:27:32 +0100

    added whatwasithinking

--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,8 @@ from django.views.generic.base import TemplateView
 class HomePageView(TemplateView):
 
     template_name = "home.html"
+
+
+class WhatWasIThinking(TemplateView):
+
+    template_name = "whatwasithinking.html"
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,12 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
-from frontend.views import HomePageView
+from frontend.views import HomePageView, WhatWasIThinking
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', HomePageView.as_view(), name='home'),
+    url(r'^whatwasithinking/', WhatWasIThinking.as_view(), name='whatwasithinking'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
