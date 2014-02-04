5df02fb416586beb6636a1a90866534751d73fc5
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-02-04 01:13:56 +0100

    Use python 3 __str__

--- frontend/models.py
+++ frontend/models.py
@@ -5,5 +5,5 @@ class CodeExamples(models.Model):
     title = models.CharField(max_length=80)
     text = models.TextField()
 
-    def __unicode__(self):
+    def __str__(self):
         return self.title
