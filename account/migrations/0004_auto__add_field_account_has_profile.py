a566567b454bb39e62c9298c93e40ea9dedce06b
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-02-09 00:15:15 +0100

    account migration

---
+++ account/migrations/0004_auto__add_field_account_has_profile.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Account.has_profile'
+        db.add_column(u'account_account', 'has_profile',
+                      self.gf('django.db.models.fields.BooleanField')(default=False),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Account.has_profile'
+        db.delete_column(u'account_account', 'has_profile')
+
+
+    models = {
+        u'account.account': {
+            'Meta': {'object_name': 'Account'},
+            'about_me': ('django.db.models.fields.TextField', [], {}),
+            'email': ('django.db.models.fields.EmailField', [], {'unique': 'True', 'max_length': '255'}),
+            'has_profile': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_admin': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'slug': ('django.db.models.fields.SlugField', [], {'max_length': '50'})
+        }
+    }
+
+    complete_apps = ['account']
