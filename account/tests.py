df26eb350038b37be2c8ec9a08bd62514c5e53f1
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-02-08 15:31:43 +0100

    first test registration

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,22 @@
+from .models import Account
 from django.test import TestCase
+from django.test.client import Client
+from django.core.urlresolvers import reverse
 
-# Create your tests here.
+
+class UserTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_register(self):
+        response = self.client.post(reverse('registration_register'),
+                                    {
+                                        'username': 'foo',
+                                        'password1': 'bar',
+                                        'password2': 'bar',
+                                        'email': 'bar@example.com',
+                                    }
+                                    )
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Account.objects.filter(is_active=True).count(), 1)
