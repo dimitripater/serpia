a1c6cd0b527c231938ac31863844316e6702f067
Dimitri Pater <dimitri@djangowebstudio.nl>
2014-02-14 11:55:39 +0100

    force insert color

--- account/migrations/0008_auto__add_color__add_field_account_color.py
+++ account/migrations/0008_auto__add_color__add_field_account_color.py
@@ -13,9 +13,10 @@ class Migration(SchemaMigration):
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
             ('color', self.gf('django.db.models.fields.CharField')(max_length=16)),
         ))
-        Color = 'account_color'
-        Color.color = "Red"
-        Color.save()
+
+        color = orm['account.Color']
+        color.color = "Red"
+        color.save()
         db.send_create_signal(u'account', ['Color'])
 
         # Adding field 'Account.color'
